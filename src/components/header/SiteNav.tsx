7620b99311857d0e4d61880e4b362606f8a74785
Mattia Sanfilippo <info@mattiasanfilippo.com>
2022-06-19 14:45:39 +0200

    feat: create contact page

--- src/components/header/SiteNav.tsx
+++ src/components/header/SiteNav.tsx
@@ -96,7 +96,7 @@ class SiteNav extends React.Component<SiteNavProps, SiteNavState> {
                   <Link to="/about" activeClassName="nav-current">About</Link>
                 </li>
                 <li role="menuitem">
-                  <Link to="#" activeClassName="nav-current">Contact</Link>
+                  <Link to="/contact" activeClassName="nav-current">Contact</Link>
                 </li>
               </ul>
               {isPost && (
